cff30cbd0214835f7124fc0c278fbc9fa2b1cea0
Mark Brian <markbkiunga2017@gmail.com>
2024-08-30 17:25:24 +0300

    Completes Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    pass
+
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        else:
+            return False
+
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        else:
+            return False
+
+    def count_sentences(self):
+        for punctuation in ["!", "?"]:
+            self.value = self.value.replace(punctuation, ". ")
+        sentences = [sentence for sentence in self.value.split(". ") if sentence]
+        print(sentences)
+        return len(sentences)
+
+
+simple_string = MyString("one. two! three?")
+simple_string.count_sentences()
